f88757ef344eb8c1356196d7950cf370347007c5
Elijah Bigham <ebigham@baileygp.com>
2023-08-15 16:09:33 -0400

    Initial build of app. Creates a json file and a csv summary for each URL (specified in source code, needs update to get URLs from user) for both mobile and desktop.

---
+++ lighthouse_util.mjs
@@ -0,0 +1,80 @@
+import * as fs from 'fs';
+import { createObjectCsvWriter } from 'csv-writer';
+import { spawn } from 'child_process';
+
+async function runLighthouse(url, flags = []) {
+  return new Promise((resolve, reject) => {
+    const lighthouseProcess = spawn('lighthouse', [url, ...flags, '--output=json']);
+
+    let lighthouseOutput = '';
+    lighthouseProcess.stdout.on('data', (data) => {
+      lighthouseOutput += data.toString();
+    });
+
+    lighthouseProcess.on('close', (code) => {
+      if (code === 0) {
+        try {
+          const report = JSON.parse(lighthouseOutput);
+          resolve(report);
+        } catch (error) {
+          reject(error);
+        }
+      } else {
+        reject(new Error(`Lighthouse process exited with code ${code}`));
+      }
+    });
+  });
+}
+
+async function main() {
+  const urls = [
+    "https://www.digitizeyourdocuments.com/case-studies/",
+    "https://www.digitizeyourdocuments.com/document-imaging/falcon/",
+  ];
+
+  const csvWriter = createObjectCsvWriter({
+    path: "lighthouse-scores.csv",
+    header: [
+      { id: "url", title: "URL" },
+      { id: "score_mobile", title: "Mobile Score" },
+      { id: "score_desktop", title: "Desktop Score" },
+    ],
+    append: true,
+  });
+
+  for (const url of urls) {
+    try {
+      // Run Lighthouse with default settings
+      const reportMobile = await runLighthouse(url);
+
+      // Run Lighthouse with "--preset desktop" flag
+      const reportDesktop = await runLighthouse(url, ['--preset', 'desktop']);
+
+      const scoreMobile = reportMobile.categories.performance.score * 100;
+      const scoreDesktop = reportDesktop.categories.performance.score * 100;
+
+      // Save the reports as JSON files
+      const filenameMobile = `${url.replace(/[^a-zA-Z0-9]/g, '_')}_mobile.json`;
+      const filenameDesktop = `${url.replace(/[^a-zA-Z0-9]/g, '_')}_desktop.json`;
+
+      fs.writeFileSync(filenameMobile, JSON.stringify(reportMobile, null, 2));
+      fs.writeFileSync(filenameDesktop, JSON.stringify(reportDesktop, null, 2));
+
+      // Write scores to CSV
+      const record = {
+        url: url,
+        score_mobile: scoreMobile,
+        score_desktop: scoreDesktop,
+      };
+
+      csvWriter
+        .writeRecords([record])
+        .then(() => console.log(`Scores and reports for ${url} written to CSV and JSON files`))
+        .catch((error) => console.error(error));
+    } catch (error) {
+      console.error(`Error running Lighthouse for ${url}:`, error);
+    }
+  }
+}
+
+main();
